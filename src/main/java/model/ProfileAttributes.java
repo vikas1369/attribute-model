a3bc651b0f6f1b7fd719fbfc8639112d642e405a
Vikas Yadav <do.vikas1369@gmail.com>
2024-02-10 14:08:40 +0000

    Attribute model version 1

---
+++ src/main/java/model/ProfileAttributes.java
@@ -0,0 +1,24 @@
+package model;
+
+public class ProfileAttributes {
+
+    String rehypothecation;
+
+    String lePay;
+
+    public String getRehypothecation() {
+        return rehypothecation;
+    }
+
+    public void setRehypothecation(String rehypothecation) {
+        this.rehypothecation = rehypothecation;
+    }
+
+    public String getLePay() {
+        return lePay;
+    }
+
+    public void setLePay(String lePay) {
+        this.lePay = lePay;
+    }
+}
---
+++ src/test/java/com/vikas/attributemodel/AttributeModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.attributemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttributeModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
